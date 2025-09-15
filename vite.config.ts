18b54cc5e57f71fdf31170fc9a8f21db0d92fb51
Karl Steinmann <steinmann@vakutec.at>
2025-09-15 05:48:58 +0000

    test geht

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  resolve: { alias: { '@': '/src' } }
-})
+  resolve: { alias: { "@": resolve(__dirname, "src") } },
+  server: { host: true, port: 5173,strictPort: true, allowedHosts: [".gitpod.io"] },
+  preview: { host: true, port: 5173 },
+  build: {
+    rollupOptions: {
+      input: {
+        admin: resolve(__dirname, "admin.html"),
+        qr: resolve(__dirname, "qr.html"),
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
